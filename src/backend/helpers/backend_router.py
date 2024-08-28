7c8a81633d633dfdb14906adb946c176e16d910f
dfavenfre <tolgasa2@gmail.com>
2024-08-28 15:42:53 +0300

    1 - Added docstring for all endpoints

--- src/backend/helpers/backend_router.py
+++ src/backend/helpers/backend_router.py
@@ -47,6 +47,40 @@ async def check_backend_status():
     status_code=status.HTTP_202_ACCEPTED
 )
 async def fetch_video_length(payload: FetchVideoLengthModel):
+    """
+    Fetches the length of a video given its URL.
+
+    Description:
+    ------------
+    This endpoint takes a video URL as input and retrieves the length of the video. The length is obtained by using the `scrape_video_length`
+    function, which extracts the duration of the video from the video page. The length is returned as a floating-point number representing
+    the duration in seconds.
+
+    Args:
+    ------------
+    `payload (FetchVideoLengthModel)`: The request payload containing the URL of the video for which the length is to be fetched. It should
+                                     be a valid URL pointing to a video.
+
+    Returns:
+    ------------
+    `float`: The length of the video in seconds. If the length is not found, an HTTP 404 error is raised.
+
+    Raises:
+    ------------
+    `HTTPException`: If an error occurs during the video length retrieval or if the video length cannot be determined, a 404 error is raised
+                   with a relevant error message.
+
+    Example:
+    ------------
+    If the `payload` contains:
+        {
+            "video_url": "https://www.youtube.com/watch?v=bG4VYwFnU8k"
+        }
+
+    And the video length is 120.5 seconds, the function returns: 120.5
+
+    If the video length cannot be determined, the function raises a 404 error.
+    """
     try:
         video_length = scrape_video_length(payload.video_url)
 
@@ -68,6 +102,38 @@ async def fetch_video_length(payload: FetchVideoLengthModel):
 async def fetch_transcription_element(
         payload: FetchTranscriptionModel
 ):
+    """
+    Checks if the video at the specified URL has a transcription available.
+
+    Description:
+    ------------
+    This endpoint takes a video URL and checks if a transcription is available for that video. It utilizes the `check_transcription`
+    function to determine the presence of a transcription. The result indicates whether a transcription can be found.
+
+    Args:
+    ------------
+    `payload (FetchTranscriptionModel)`: The request payload containing the URL of the video to check. It should be a valid URL
+                                       pointing to a video, encapsulated in a `FetchTranscriptionModel` instance.
+
+    Returns:
+    ------------
+    `bool`: Returns `True` if a transcription is found, otherwise returns `False`.
+
+    Raises:
+    ------------
+    `HTTPException`: If an error occurs during the transcription check, a 422 error is raised with the error message.
+
+    Example:
+    ------------
+    If the `payload` contains:
+        {
+            "video_url": "https://www.youtube.com/watch?v=bG4VYwFnU8k"
+        }
+
+    And a transcription is found, the function returns: `True`
+
+    If no transcription is found, the function returns: `False`
+    """
     try:
         found_transcription = check_transcription(payload.video_url)
 
@@ -101,6 +167,40 @@ async def fetch_video_transcription(
             }
         )
 ) -> Dict[str, str]:
+    """
+    Scrapes the full transcription of a video given its URL.
+
+    Description:
+    ------------
+    This endpoint takes a video URL as input and returns the full transcription of the video. The transcription is scraped
+    from the video and returned in a dictionary where keys are timestamps and values are the corresponding transcription text.
+
+    Args:
+    ------------
+    `video_url (str)`: The URL of the video to scrape the transcription from. It should be a valid URL pointing to a video,
+                     with a minimum length of 10 characters and a maximum length of 100 characters.
+
+    Returns:
+    ------------
+    `Dict[str, str]`: A dictionary containing the full transcription of the video. Each key is a timestamp in the format "MM:SS",
+                    and each value is the corresponding transcription text.
+
+    Raises:
+    ------------
+    `HTTPException`: If an error occurs during the transcription scraping process, a 404 error is raised with the error message.
+
+    Example:
+    ------------
+    If the `video_url` is:
+    "https://www.youtube.com/watch?v=bG4VYwFnU8k&t=5s".
+
+    The function returns:
+        {
+            "00:05": "Introduction",
+            "00:30": "Main discussion starts",
+            "01:00": "Conclusion"
+        }
+    """
     try:
         full_transcription = scrape_transcription(video_url)
 
@@ -170,7 +270,7 @@ async def converse_with_agent(
                 return response
 
     except Exception as err:
-        raise HTTPException(status_code=404, detail=str(err))
+        raise HTTPException(status_code=500, detail=str(err))
 
 
 @functions_router.post(
@@ -185,32 +285,42 @@ async def chat_with_transcriptions(
             max_length=250,
             example=["Would you tell me which version of the software was supposed to be installed?"]
         )
-) -> str:
+) -> Dict[str, Any]:
     """
+    Chat with the scraped video transcription.
+
     Description:
     -------------
-    Allows user to chat with the scraped video transcription, that are embedded into
-    a vectorstore for RAG process.
+    This endpoint allows users to interact with the transcription of a video by sending a chat message. The `rag_tool` processes the chat
+    message, retrieves relevant information from the video transcription, and returns a response.
 
     Args:
     -------------
-    `chat_message: str` = The chat message sent from the user
+    `chat_message (str)`: The user's chat message. This message will be used by the `rag_tool` to query the video transcription and
+                        generate a response. The message must be between 5 and 250 characters long.
 
     Returns:
     -------------
-    A response based on the result of RAG process
+    `Dict[str, Any]`: A dictionary containing the response generated by the `rag_tool` based on the chat message and the video transcription.
+                    If no response is generated, the function raises an HTTP 404 error.
+
+    Raises:
+    -------------
+    `HTTPException`: If an error occurs during the processing of the chat message or if no response can be generated, an HTTP 404 error
+                   is raised with a relevant error message.
     """
     try:
         response = rag_tool._run(chat_message)
         if response is not None:
             return response
+
     except Exception as err:
         raise HTTPException(status_code=404, detail=str(err))
 
 
 @functions_router.post(
     path="/process/identify_request",
-    summary='Endpoint for identifying request as either "text" or "image".',
+    summary='Endpoint for identifying request as either "information" or "image".',
     status_code=status.HTTP_202_ACCEPTED
 )
 async def identify_request(
@@ -222,17 +332,24 @@ async def identify_request(
         )
 ) -> Dict[str, Any]:
     """
+    Identifies the type of user request as either 'information' or 'image'.
+
     Description:
     ------------
-    Identifies whether the provided `chat_message` is a text-based or image-based request.
+    This endpoint takes a user's chat message and uses the `request_identifier`
+    tool to determine whether the request is related to text information or an image.
 
     Args:
     ------------
-    `chat_message (str)`: The user's message that needs to be identified as either text or image.
+    `chat_message: str`: The user's chat message to be analyzed. Must be between 10 and 250 characters long.
 
     Return:
     ------------
-    `Dict[str, Any]`: A dictionary with the key 'type' indicating whether the request is "text" or "image".
+    `Dict[str, Any]`: A dictionary containing the identified request type, either "information" or "image".
+
+    Raises:
+    ------------
+    `HTTPException`: If an error occurs during request processing, an HTTP 422 error is raised.
     """
     try:
         response = request_identifier._run(chat_message)
