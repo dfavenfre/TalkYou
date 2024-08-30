aa270e3ba9d73d6660e5dfd77fc80a997e691231
dfavenfre <tolgasa2@gmail.com>
2024-08-30 15:11:36 +0300

    1 - Removed render_chat() method for displaying chat container. Now Streamlit-native  chat_message component is utilized.

--- src/frontend/talkyou.py
+++ src/frontend/talkyou.py
@@ -1,16 +1,12 @@
-import base64
-
 from helpers.helper_functions import (
     load_sidebar_html,
-    render_chat,
     chat_bot,
-    submit_video_url
+    submit_video_url,
+    render_landing_page
 )
-from streamlit_extras.stylable_container import stylable_container
-import extra_streamlit_components as esc
 import streamlit as st
-import time
 import asyncio
+import base64
 
 st.set_page_config(
     page_title="TalkYou - Talk With Videos!",
@@ -23,15 +19,12 @@ st.set_page_config(
 if "video_url" not in st.session_state:
     st.session_state["video_url"] = None
 
-if "last_rendered_index" not in st.session_state:
-    st.session_state["last_rendered_index"] = 0
-
 with st.sidebar:
     sidebar_content = load_sidebar_html()
     st.html(sidebar_content)
 
 if st.session_state["video_url"] is None:
-    st.image("helpers/elements/assets/talkyou-langgraph-schema.jpg")
+    render_landing_page()
     with st.expander(label="Welcome To TalkYou 👋", expanded=True):
         video_url = st.text_input("Have any videos on your mind?")
         if st.button(
@@ -44,9 +37,15 @@ elif st.session_state.video_url is not None:
 
     if "messages" not in st.session_state:
         st.session_state["messages"] = [
-            {"role": "assistant", "content": "Welcome, It's Your Video Talking! What do you like to ask me?"}
+            {
+                "role": "assistant",
+                "content": "Welcome, It's Your Video Talking! What do you like to ask me?"
+            }
         ]
-    render_chat()
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
     if user_message := st.chat_input(
             placeholder="What do you like to ask me?",
@@ -55,19 +54,23 @@ elif st.session_state.video_url is not None:
     ):
 
         st.session_state.messages.append({"role": "user", "content": user_message})
-        render_chat()
+        with st.chat_message("user"):
+            st.write(user_message)
 
-        ai_message = asyncio.run(
-            chat_bot(
-                video_url=st.session_state.video_url,
-                chat_message=user_message
+        with st.spinner("Looking for an answer..."):
+            ai_message = asyncio.run(
+                chat_bot(
+                    video_url=st.session_state.video_url,
+                    chat_message=user_message
+                )
             )
-        )
 
-        if "screenshot_base64" in ai_message.keys() and ai_message["identified_request"] == "image":
-            converted_image = base64.b64decode(ai_message["screenshot_base64"])
-            st.image(converted_image)
+            if "screenshot_base64" in ai_message.keys() and ai_message["identified_request"] == "image":
+                converted_image = base64.b64decode(ai_message["screenshot_base64"])
+                with st.chat_message("assistant"):
+                    st.image(converted_image)
 
-        else:
-            st.session_state.messages.append({"role": "assistant", "content": ai_message["response"]})
-            render_chat()
+            else:
+                st.session_state.messages.append({"role": "assistant", "content": ai_message["response"]})
+                with st.chat_message("assistant"):
+                    st.write(ai_message["response"])
